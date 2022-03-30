0cfc58e63ed7e100cece8c8c92a5462f532886cd
jaegwang Kim <66285984+kimjaegwang0408@users.noreply.github.com>
2022-03-30 16:20:46 +0900

    Update main.js

--- portfolio.v2/js/main.js
+++ portfolio.v2/js/main.js
@@ -1,1 +1,18 @@
 
+$(function () {
+    $(".Anchor").on("click", function(){
+        var headerHeight = $(".navbar").outerHeight();
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "body" : href);
+        var position = target.offset().top - headerHeight;
+        $("html, body").animate({ scrollTop: position }, 600, "swing");
+    });
+});
+
+
+$(function () {
+    $('li').on('click', function(){
+        $('li').removeClass('active');
+        $(this).addClass('active');
+        });
+});
